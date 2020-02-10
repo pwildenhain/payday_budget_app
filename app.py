664b3d6d09f2a7e9898bafdccdcfc3f1bc99cd26
pwildenhain <pwildenhain@gmail.com>
2020-02-09 22:38:31 -0500

    :recycle: Add account row

--- app.py
+++ app.py
@@ -1,18 +1,38 @@
 import wx
 
 
-class MainWindow(wx.Frame):
+class BudgetFrame(wx.Frame):
     def __init__(self):
-        super().__init__(parent=None, title="Hello Bre!")
+        super().__init__(parent=None, title="Payday Budget")
         panel = wx.Panel(self)
+        sizer = wx.BoxSizer(wx.HORIZONTAL)
 
-        self.text_ctrl = wx.TextCtrl(panel, pos=(5, 5))
-        btn = wx.Button(panel, label="PRESS ME", pos=(5, 55))
+        self.account_name = wx.StaticText(panel, label="Transportation")
+        sizer.Add(self.account_name, 0, wx.ALL, 5)
+
+        self.tx_amount = wx.TextCtrl(panel)
+        sizer.Add(self.tx_amount, 0, wx.ALL, 5)
+
+        self.tx_comment = wx.TextCtrl(panel)
+        sizer.Add(self.tx_comment, 0, wx.ALL, 5)
+
+        btn = wx.Button(panel, label="Add Transaction")
+        btn.Bind(wx.EVT_BUTTON, self.on_press)
+        sizer.Add(btn, 0, wx.ALL, 5)
+
+        panel.SetSizer(sizer)
 
         self.Show()
+
+    def on_press(self, event):
+        amount = self.tx_amount.GetValue()
+        comment = self.tx_comment.GetValue()
+        print(f"${amount} on {comment}") if amount else print(
+            "You didn't type anything..."
+        )
 
 
 if __name__ == "__main__":
     app = wx.App()
-    frame = MainWindow()
+    frame = BudgetFrame()
     app.MainLoop()
