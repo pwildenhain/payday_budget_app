d3013339c8fd6f4470c66a48325a06c50c1f6fd5
pwildenhain <pwildenhain@gmail.com>
2020-02-15 15:50:46 -0500

    :recycle: Add List Ctrl + Button Opts

--- app.py
+++ app.py
@@ -1,38 +1,60 @@
 import wx
 
 
-class BudgetFrame(wx.Frame):
+class BudgetPanel(wx.Panel):
+    def __init__(self, parent):
+        super().__init__(parent)
+        top_options_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        main_sizer = wx.BoxSizer(wx.VERTICAL)
+
+        self.add_account_btn = wx.Button(self, label="Add Account")
+        self.add_account_btn.Bind(wx.EVT_BUTTON, self.handle_add_account)
+        top_options_sizer.Add(self.add_account_btn, 0, wx.ALL, 5)
+
+        self.record_payday_btn = wx.Button(self, label="Record Payday")
+        self.record_payday_btn.Bind(wx.EVT_BUTTON, self.handle_record_payday)
+        top_options_sizer.Add(self.record_payday_btn, 0, wx.ALL, 5)
+
+        self.view_tx_history_btn = wx.Button(self, label="View Transaction History")
+        self.view_tx_history_btn.Bind(wx.EVT_BUTTON, self.handle_view_tx_history)
+        top_options_sizer.Add(self.view_tx_history_btn, 0, wx.ALL, 5)
+
+        self.list_ctrl = wx.ListCtrl(
+            self, size=(-1, 100), style=wx.LC_REPORT | wx.BORDER_SUNKEN
+        )
+
+        self.list_ctrl.InsertColumn(0, "Category", width=150)
+        self.list_ctrl.InsertColumn(1, "Name", width=150)
+        self.list_ctrl.InsertColumn(2, "Budgeted Amount", width=150)
+        self.list_ctrl.InsertColumn(3, "Curent Amount", width=150)
+        self.list_ctrl.InsertColumn(4, "Transaction Amount", width=150)
+        self.list_ctrl.InsertColumn(5, "Transaction Comment", width=150)
+        self.list_ctrl.InsertColumn(6, "Add Transaction", width=150)
+        self.list_ctrl.InsertColumn(7, "Modify Account", width=150)
+
+        main_sizer.Add(top_options_sizer, 0, wx.ALL | wx.EXPAND)
+        main_sizer.Add(self.list_ctrl, 0, wx.ALL | wx.EXPAND)
+
+        self.SetSizer(main_sizer)
+
+    def handle_add_account(self, event):
+        print("New account added!")
+
+    def handle_record_payday(self, event):
+        print("$$ Cha-Ching $$")
+
+    def handle_view_tx_history(self, event):
+        print("Viewing Transactions")
+
+
+class AppFrame(wx.Frame):
     def __init__(self):
         super().__init__(parent=None, title="Payday Budget")
-        panel = wx.Panel(self)
-        sizer = wx.BoxSizer(wx.HORIZONTAL)
-
-        self.account_name = wx.StaticText(panel, label="Transportation")
-        sizer.Add(self.account_name, 0, wx.ALL, 5)
-
-        self.tx_amount = wx.TextCtrl(panel)
-        sizer.Add(self.tx_amount, 0, wx.ALL, 5)
-
-        self.tx_comment = wx.TextCtrl(panel)
-        sizer.Add(self.tx_comment, 0, wx.ALL, 5)
-
-        btn = wx.Button(panel, label="Add Transaction")
-        btn.Bind(wx.EVT_BUTTON, self.on_press)
-        sizer.Add(btn, 0, wx.ALL, 5)
-
-        panel.SetSizer(sizer)
-
+        panel = BudgetPanel(self)
         self.Show()
-
-    def on_press(self, event):
-        amount = self.tx_amount.GetValue()
-        comment = self.tx_comment.GetValue()
-        print(f"${amount} on {comment}") if amount else print(
-            "You didn't type anything..."
-        )
 
 
 if __name__ == "__main__":
     app = wx.App()
-    frame = BudgetFrame()
+    frame = AppFrame()
     app.MainLoop()
